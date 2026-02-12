9edcd8a1527561cbed4f2fbc17bd0b7d0cb675fd
Aurora Scharff <aurora.sofie@gmail.com>
2026-02-12 02:00:00 +0100

    Add staletimes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   cacheComponents: true,
   experimental: {
     authInterrupts: true,
+    staleTimes: {
+      dynamic: 30,
+    },
   },
   reactCompiler: true,
   typedRoutes: true,
